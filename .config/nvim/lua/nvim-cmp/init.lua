eb7697e0f454c8294883aea1afff111875fdfcd4
pxtchphrxse <phatcharaphon.phromjom@gmail.com>
2023-01-20 20:03:40 +0700

    change cmp source order

--- .config/nvim/lua/nvim-cmp/init.lua
+++ .config/nvim/lua/nvim-cmp/init.lua
@@ -67,9 +67,9 @@ cmp.setup({
 		}),
 	}),
 	sources = cmp.config.sources({
-		{ name = "luasnip" },
 		{ name = "nvim_lsp" },
 		{ name = "nvim_lsp_signature_help" },
+		{ name = "luasnip" },
 		{ name = "buffer" },
 		{ name = "path" },
 	}),
