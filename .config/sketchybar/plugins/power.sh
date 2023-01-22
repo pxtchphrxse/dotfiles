16bb76164df893f01cecd263b3e6650e191e64fc
pxtchphrxse <phatcharaphon.phromjom@gmail.com>
2023-01-22 13:25:08 +0700

    update sketchybar battery widget

--- .config/sketchybar/plugins/power.sh
+++ .config/sketchybar/plugins/power.sh
@@ -3,25 +3,25 @@
 BATT_PERCENT=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
 CHARGING=$(pmset -g batt | grep 'AC Power')
 
-if [[ ${CHARGING} != "" ]]; then
-  sketchybar --set ${NAME} icon="􀢋"
-  sketchybar --set ${NAME} label="${BATT_PERCENT}%"
-  exit 0
-fi
+COLOR=white
+BATTERY_100=􀛨
+BATTERY_75=􀺸
+BATTERY_50=􀺶
+BATTERY_25=􀛩
+BATTERY_0=􀛪
+BATTERY_CHARGING=􀢋
 
 case ${BATT_PERCENT} in
-    100) ICON="􀛨" ;;
-    9[0-9]) ICON="􀛨" ;;
-    8[0-9]) ICON="􀛨" ;;
-    7[0-9]) ICON="􀛨" ;;
-    6[0-9]) ICON="􀛨" ;;
-    5[0-9]) ICON="􀛨" ;;
-    4[0-9]) ICON="􀛨" ;;
-    3[0-9]) ICON="􀛨" ;;
-    2[0-9]) ICON="􀛨" ;;
-    1[0-9]) ICON="􀛨" ;;
-    *) ICON="􀛨"
+    9[0-9]|100) ICON=$BATTERY_100 ;;
+    [6-8][0-9]) ICON=$BATTERY_75 ;;
+    [3-5][0-9]) ICON=$BATTERY_50 ;;
+    [1-2][0-9]) ICON=$BATTERY_25; COLOR=$ORANGE ;;
+    *) ICON=$BATTERY_0; COLOR=$RED
 esac
+
+if [[ ${CHARGING} != "" ]]; then
+    ICON="${BATTERY_CHARGING}"
+fi
 
 sketchybar --set ${NAME} icon="${ICON}"
 sketchybar --set ${NAME} label="${BATT_PERCENT}%"
