ab775e51ce3906911d14636f930b15ece1d755ea
pxtchphrxse <phatcharaphon.phromjom@gmail.com>
2023-01-22 13:55:52 +0700

    update lualine and tmux style

--- .config/nvim/lua/plugins/configs/lualine.lua
+++ .config/nvim/lua/plugins/configs/lualine.lua
@@ -7,10 +7,10 @@ lualine.setup({
 	options = {
 		icons_enabled = true,
 		theme = "onedark_dark",
-		section_separators = { left = "", right = "" },
-		component_separators = { left = "", right = "" },
-		-- section_separators = { left = "", right = "" },
-		-- component_separators = { left = "", right = "" },
+		-- section_separators = { left = "", right = "" },
+		-- component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
+		component_separators = { left = "", right = "" },
 		disabled_filetypes = {},
 	},
 	sections = {
