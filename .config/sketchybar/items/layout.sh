db5297056eaa0d16c184dee29b95f9fc1b19f998
pxtchphrxse <phatcharaphon.phromjom@gmail.com>
2023-02-02 21:55:57 +0700

    optimize sbar input source updating by OS event

--- .config/sketchybar/items/layout.sh
+++ .config/sketchybar/items/layout.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env sh
 
-sketchybar -m --add item layout right \
-    --set layout update_freq=1 \
+sketchybar \
+    --add event layout_changed AppleSelectedInputSourcesChangedNotification \
+    --add item layout right \
+    --set layout \
+    updates=on \
     script="$PLUGIN_DIR/im-select.sh" \
     label.color=0xFFFFFFFF \
     label.font="JetBrains Mono:BOLD:13.0" \
@@ -9,4 +12,5 @@ sketchybar -m --add item layout right \
     background.height=24 \
     background.corner_radius=4 \
     label.padding_left=0 \
-    label.padding_right=9
+    label.padding_right=9 \
+    --subscribe layout layout_changed
