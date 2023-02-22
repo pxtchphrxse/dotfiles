be39e2d001da3bc0720938171950193b2c1f3dce
pxtchphrxse <phatcharaphon.phromjom@gmail.com>
2023-02-22 14:12:30 +0700

    add leap.nvim enhanced f/t motion

--- .config/nvim/lua/plugins/motion/init.lua
+++ .config/nvim/lua/plugins/motion/init.lua
@@ -21,5 +21,17 @@ return {
 			require("plugins.motion.leap-setup").setup(opts)
 		end,
 	},
+	{
+		"ggandor/flit.nvim",
+		keys = function()
+			---@type LazyKeys[]
+			local ret = {}
+			for _, key in ipairs({ "f", "F", "t", "T" }) do
+				ret[#ret + 1] = { key, mode = { "n", "x", "o" }, desc = key }
+			end
+			return ret
+		end,
+		opts = { labeled_modes = "nx" },
+	},
 	{ "windwp/nvim-spectre", cmd = "Spectre" },
 }
