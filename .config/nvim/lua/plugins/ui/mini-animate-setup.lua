d951da0dccec26594cb2bf1669949e9b8086d8c3
pxtchphrxse <phatcharaphon.phromjom@gmail.com>
2023-12-27 08:02:46 +0700

    disable some mini.animate features

--- .config/nvim/lua/plugins/ui/mini-animate-setup.lua
+++ .config/nvim/lua/plugins/ui/mini-animate-setup.lua
@@ -15,18 +15,9 @@ local opts = {
   resize = {
     timing = animate.gen_timing.linear({ duration = 50, unit = "total" }),
   },
-  scroll = {
-    timing = animate.gen_timing.linear({ duration = 80, unit = "total" }),
-    subscroll = animate.gen_subscroll.equal({
-      predicate = function(total_scroll)
-        if mouse_scrolled then
-          mouse_scrolled = false
-          return false
-        end
-        return total_scroll > 1
-      end,
-    }),
-  },
+  scroll = { enable = false },
+  open = { enable = false },
+  close = { enable = false },
 }
 
 animate.setup(opts)
