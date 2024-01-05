b5105247b73837b8240833193f6cf1beabfe3857
pxtchphrxse <phatcharaphon.phromjom@gmail.com>
2024-01-05 14:10:36 +0700

    update neo-tree config

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -8,7 +8,7 @@ return {
     },
     config = function()
       require("neo-tree").setup({
-        close_if_last_window = true,
+        close_if_last_window = false,
         default_component_configs = {
           git_status = {
             symbols = {
@@ -27,6 +27,7 @@ return {
           },
         },
         window = {
+          position = "current",
           mappings = {
             ["l"] = "open",
             ["h"] = "close_node",
