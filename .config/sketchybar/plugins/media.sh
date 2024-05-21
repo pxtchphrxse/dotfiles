06c57cf333ba1e088e94fa2ae9a9b39c66d7b39b
pxtchphrxse <phatcharaphon.phromjom@gmail.com>
2024-05-21 14:14:27 +0700

    update sketchybar config

--- .config/sketchybar/plugins/media.sh
+++ .config/sketchybar/plugins/media.sh
@@ -2,70 +2,74 @@
 # require nowplaying-cli (https://github.com/kirtan-shah/nowplaying-cli.git)
 
 next() {
-    nowplaying-cli next
+	nowplaying-cli next
 }
 back() {
-    nowplaying-cli back
+	nowplaying-cli back
 }
 play() {
-    nowplaying-cli togglePlayPause
+	nowplaying-cli togglePlayPause
 }
 
 update() {
-    PLAYBACK_RATE=$(nowplaying-cli get playbackRate)
-    if [ $PLAYBACK_RATE = 1 ]
-    then
-        sketchybar --set music.play icon="󰏤" icon.font.size=19.0
-    else
-        sketchybar --set music.play icon="" icon.font.size=14.0
-    fi
+	PLAYBACK_RATE=$(nowplaying-cli get playbackRate)
+	if [ $PLAYBACK_RATE = 1 ]; then
+		sketchybar --set music.play icon="󰏤" icon.font.size=19.0
+	else
+		sketchybar --set music.play icon="" icon.font.size=14.0
+	fi
 
-    TRACK_NAME=$(echo $INFO | jq -r ".title")
-    TRACK_ARTIST=$(echo $INFO | jq -r ".artist")
-    if [[ "$TRACK_NAME" = "null" || $TRACK_ARTIST = "" ]]
-    then
-        PLAYER_LABEL=""
-        sketchybar --set music.play label="$PLAYER_LABEL" label.padding_right=0
-        return
-    else
-        if [[ "$TRACK_ARTIST" = "null" || "$TRACK_ARTIST" = "" ]]
-        then
-            PLAYER_LABEL="$TRACK_NAME"
-        elif [[ "$TRACK_NAME" = "null" || "$TRACK_NAME" = "" ]]
-        then
-            PLAYER_LABEL="$TRACK_ARTIST"
-        else
-            PLAYER_LABEL="$TRACK_NAME - $TRACK_ARTIST"
-        fi
-        sketchybar --set music.play label="$PLAYER_LABEL" label.padding_right=8
-    fi
+	TRACK_NAME=$(nowplaying-cli get title)
+	TRACK_ARTIST=$(nowplaying-cli get artist)
+	if [[ "$TRACK_NAME" = "null" || $TRACK_ARTIST = "" ]]; then
+		PLAYER_LABEL=""
+		sketchybar --set music.play label="$PLAYER_LABEL" label.padding_right=0
+		return
+	else
+		if [[ "$TRACK_ARTIST" = "null" || "$TRACK_ARTIST" = "" ]]; then
+			PLAYER_LABEL="$TRACK_NAME"
+		elif [[ "$TRACK_NAME" = "null" || "$TRACK_NAME" = "" ]]; then
+			PLAYER_LABEL="$TRACK_ARTIST"
+		else
+			PLAYER_LABEL="$TRACK_NAME - $TRACK_ARTIST"
+		fi
+		sketchybar --set music.play label="$PLAYER_LABEL" label.padding_right=8
+	fi
 }
 
-mouse_clicked () {
-    case "$NAME" in
-        "music.next") next
-            ;;
-        "music.back") back
-            ;;
-        "music.play") play
-            ;;
-        *) exit
-            ;;
-    esac
+mouse_clicked() {
+	case "$NAME" in
+	"music.next")
+		next
+		;;
+	"music.back")
+		back
+		;;
+	"music.play")
+		play
+		;;
+	*)
+		exit
+		;;
+	esac
 }
 
 routine() {
-    case "$NAME" in
-        *) update
-            ;;
-    esac
+	case "$NAME" in
+	*)
+		update
+		;;
+	esac
 }
 
 case "$SENDER" in
-    "mouse.clicked") mouse_clicked
-        ;;
-    "routine") routine
-        ;;
-    *) update
-        ;;
+"mouse.clicked")
+	mouse_clicked
+	;;
+"routine")
+	routine
+	;;
+*)
+	update
+	;;
 esac
