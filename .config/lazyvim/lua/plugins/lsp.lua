9cd96408b1886de9bbd0af763b8e273599323238
pxtchphrxse <phatcharaphon.phromjom@gmail.com>
2024-10-30 20:12:52 +0700

    add additional configuration to eslint

--- .config/lazyvim/lua/plugins/lsp.lua
+++ .config/lazyvim/lua/plugins/lsp.lua
@@ -82,6 +82,18 @@ return {
           settings = {
             -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
             workingDirectories = { mode = "auto" },
+            rulesCustomizations = {
+              { rule = "style/*", severity = "off", fixable = true },
+              { rule = "format/*", severity = "off", fixable = true },
+              { rule = "*-indent", severity = "off", fixable = true },
+              { rule = "*-spacing", severity = "off", fixable = true },
+              { rule = "*-spaces", severity = "off", fixable = true },
+              { rule = "*-order", severity = "off", fixable = true },
+              { rule = "*-dangle", severity = "off", fixable = true },
+              { rule = "*-newline", severity = "off", fixable = true },
+              { rule = "*quotes", severity = "off", fixable = true },
+              { rule = "*semi", severity = "off", fixable = true },
+            },
           },
         },
         tailwindcss = {},
