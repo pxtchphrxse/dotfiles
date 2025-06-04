21bdfadedf3d5cc9791776a9b99edd6218d024a6
pxtchphrxse <phatcharaphon.phromjom@gmail.com>
2025-06-04 23:53:27 +0700

    disable distracted default lualine diagenostics

---
+++ .config/lazyvim/lua/plugins/ui.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      sections = {
+        lualine_c = {
+          LazyVim.lualine.root_dir(),
+          -- hide default context since its already in top section
+          -- {
+          --   "diagnostics",
+          --   symbols = {
+          --     error = icons.diagnostics.Error,
+          --     warn = icons.diagnostics.Warn,
+          --     info = icons.diagnostics.Info,
+          --     hint = icons.diagnostics.Hint,
+          --   },
+          -- },
+          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+          { LazyVim.lualine.pretty_path() },
+        },
+      },
+    },
+  },
+}
