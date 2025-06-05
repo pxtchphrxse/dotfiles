f9936844f66985f1b9217d68e7bc650ec3e45909
pxtchphrxse <phatcharaphon.phromjom@gmail.com>
2025-06-05 21:08:45 +0700

    change theme to transparent background

--- .config/lazyvim/lua/plugins/color.lua
+++ .config/lazyvim/lua/plugins/color.lua
@@ -4,7 +4,11 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
+      require("onedarkpro").setup({
+        options = { transparency = true },
+      })
       vim.cmd([[colorscheme onedark_dark]])
+      -- vim.cmd([[hi CursorLine guibg=none]])
       -- vim.cmd([[hi CursorLine guibg=#16181d]])
     end,
   },
